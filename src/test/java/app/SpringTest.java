c0117c3215b96d797979cc7a78d7e5635aaa3d9b
LiJohnson <598420668@163.com>
2016-01-18 21:41:24 +0800

    remove useless code

--- src/test/java/app/SpringTest.java
+++ src/test/java/app/SpringTest.java
@@ -1,30 +1,5 @@
 package app;
 
-import com.google.gson.Gson;
-import io.lcs.lenrui.module.main.service.DeviceService;
-import io.lcs.lenrui.module.sms.pojo.SMS;
-import io.lcs.lenrui.module.sms.service.SMSService;
-import io.lcs.lenrui.module.sms.vo.CodeVo;
-import io.lcs.lenrui.pojo.*;
-import io.lcs.lenrui.config.Http;
-import io.lcs.lenrui.dao.BrandRepository;
-import io.lcs.lenrui.dao.ProductRepository;
-import io.lcs.lenrui.service.AdminService;
-import io.lcs.lenrui.service.OptionService;
-import io.lcs.lenrui.service.ProductService;
-import io.lcs.lenrui.vo.PageVo;
-import org.apache.http.message.BasicNameValuePair;
-import org.dom4j.DocumentException;
-import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-
-import java.io.UnsupportedEncodingException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * Created by lcs on 15-5-30.
